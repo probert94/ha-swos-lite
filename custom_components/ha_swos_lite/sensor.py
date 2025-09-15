ea6395587ccfe828cf59bb7ed4e128781d049714
Robert Pattis <pattis_r@hotmail.de>
2025-09-15 21:43:11 +0200

    Translate entities
    - Translate default entity names
    - Add german translations
    - Use entity_description

--- custom_components/ha_swos_lite/sensor.py
+++ custom_components/ha_swos_lite/sensor.py
@@ -2,9 +2,12 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
     SensorStateClass,
 )
 from homeassistant.const import (
@@ -20,6 +23,48 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import ATTR_MANUFACTURER, DOMAIN
 from .coordinator import MikrotikSwosLiteConfigEntry, MikrotikSwosLiteCoordinator
 from .port import Port
+
+
+@dataclass(frozen=True, kw_only=True)
+class MikrotikSwOSLiteEntityDescription(SensorEntityDescription):
+    """Describes a Mikrotik SwitchOS Lite Sensor."""
+
+    endpoint: str
+    property: str
+
+
+PORT_SENSORS: tuple[MikrotikSwOSLiteEntityDescription, ...] = (
+    MikrotikSwOSLiteEntityDescription(
+        key="poe_power",
+        translation_key="poe_power",
+        device_class=SensorDeviceClass.POWER,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfPower.WATT,
+        suggested_display_precision=1,
+        endpoint="poe",
+        property="power",
+    ),
+    MikrotikSwOSLiteEntityDescription(
+        key="poe_current",
+        translation_key="poe_current",
+        device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfElectricCurrent.MILLIAMPERE,
+        suggested_display_precision=1,
+        endpoint="poe",
+        property="current",
+    ),
+    MikrotikSwOSLiteEntityDescription(
+        key="poe_voltage",
+        translation_key="poe_voltage",
+        device_class=SensorDeviceClass.VOLTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        suggested_display_precision=1,
+        endpoint="poe",
+        property="voltage",
+    ),
+)
 
 
 async def async_setup_entry(
@@ -40,90 +85,47 @@ async def async_setup_entry(
         "sw_version": coordinator.firmware,
     }
 
-    port_sensors = [
-        MikrotikSwosLitePoEPowerSensor,
-        MikrotikSwosLitePoECurrentSensor,
-        MikrotikSwosLitePoEVoltageSensor,
-    ]
     async_add_entities(
         [
-            port_sensor(coordinator, device, port)
-            for port_sensor in port_sensors
+            MikrotikSwosLitePortSensor(coordinator, device, port_sensor, port)
+            for port_sensor in PORT_SENSORS
             for port in coordinator.api.ports
         ]
     )
 
 
-class MikrotikSwosLitePoEPowerSensor(
+class MikrotikSwosLitePortSensor(
     CoordinatorEntity[MikrotikSwosLiteCoordinator], SensorEntity
 ):
-    """Representation of a Mikrotik SwitchOS Lite PoE Power Sensor."""
+    """Representation of a Mikrotik SwitchOS Lite Port Sensor."""
+
+    entity_description: MikrotikSwOSLiteEntityDescription
 
     def __init__(
-        self, coordinator: MikrotikSwosLiteCoordinator, device: DeviceInfo, port: Port
+        self,
+        coordinator: MikrotikSwosLiteCoordinator,
+        device: DeviceInfo,
+        entity_description: MikrotikSwOSLiteEntityDescription,
+        port: Port,
     ) -> None:
         """Initialize the sensor entity."""
         super().__init__(coordinator)
         self.port = port
-        self._attr_name = f"P{(port.num + 1):02d} - {port.name} Power"
-        self._attr_native_unit_of_measurement = UnitOfPower.WATT
-        self._attr_suggested_display_precision = 1
-        self._attr_device_class = SensorDeviceClass.POWER
-        self._attr_state_class = SensorStateClass.MEASUREMENT
-        self._attr_unique_id = f"{coordinator.serial_num}_{port.num}_poe_power"
+        self.entity_description = entity_description
+        self.has_entity_name = True
+        self._attr_translation_placeholders = {
+            "port_num": f"{(port.num + 1):02d}",
+            "port_name": port.name,
+        }
+        self._attr_unique_id = (
+            f"{coordinator.serial_num}_{port.num}_{entity_description.key}"
+        )
         self._attr_device_info = device
 
     @property
     def native_value(self) -> float:
         """Returns the current value from the API."""
-        return self.coordinator.api.poe.power[self.port.num]
-
-
-class MikrotikSwosLitePoECurrentSensor(
-    CoordinatorEntity[MikrotikSwosLiteCoordinator], SensorEntity
-):
-    """Representation of a Mikrotik SwitchOS Lite PoE Current Sensor."""
-
-    def __init__(
-        self, coordinator: MikrotikSwosLiteCoordinator, device: DeviceInfo, port: Port
-    ) -> None:
-        """Initialize the sensor entity."""
-        super().__init__(coordinator)
-        self.port = port
-        self._attr_name = f"P{(port.num + 1):02d} - {port.name} Current"
-        self._attr_native_unit_of_measurement = UnitOfElectricCurrent.MILLIAMPERE
-        self._attr_suggested_display_precision = 1
-        self._attr_device_class = SensorDeviceClass.CURRENT
-        self._attr_state_class = SensorStateClass.MEASUREMENT
-        self._attr_unique_id = f"{coordinator.serial_num}_{port.num}_poe_current"
-        self._attr_device_info = device
-
-    @property
-    def native_value(self) -> float:
-        """Returns the current value from the API."""
-        return self.coordinator.api.poe.current[self.port.num]
-
-
-class MikrotikSwosLitePoEVoltageSensor(
-    CoordinatorEntity[MikrotikSwosLiteCoordinator], SensorEntity
-):
-    """Representation of a Mikrotik SwitchOS Lite PoE Current Sensor."""
-
-    def __init__(
-        self, coordinator: MikrotikSwosLiteCoordinator, device: DeviceInfo, port: Port
-    ) -> None:
-        """Initialize the sensor entity."""
-        super().__init__(coordinator)
-        self.port = port
-        self._attr_name = f"P{(port.num + 1):02d} - {port.name} Voltage"
-        self._attr_native_unit_of_measurement = UnitOfElectricPotential.VOLT
-        self._attr_suggested_display_precision = 1
-        self._attr_device_class = SensorDeviceClass.VOLTAGE
-        self._attr_state_class = SensorStateClass.MEASUREMENT
-        self._attr_unique_id = f"{coordinator.serial_num}_{port.num}_poe_voltage"
-        self._attr_device_info = device
-
-    @property
-    def native_value(self) -> float:
-        """Returns the current value from the API."""
-        return self.coordinator.api.poe.voltage[self.port.num]
+        return getattr(
+            getattr(self.coordinator.api, self.entity_description.endpoint),
+            self.entity_description.property,
+        )[self.port.num]
